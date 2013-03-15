5330a181f69896ae84d06f244149bf1a6c661778
richarddanbury <richard@salesmaster.co.uk>
2013-03-15 15:22:00 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DanburyBrown::Application.config.secret_token = '2b054c88346e934d969490ebfb03483e3b1774d9f5e9fd7ddb02195a64d68767e92addf07f785d4503c8d69a487abc4d44e1951c720bbfb91e579294d2e54576'
